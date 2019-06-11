dca8073bf7b6a955a6adcad204fbe9aa6ebd25f4
MichaelGonzalez <mgonzalez14291@gmail.com>
2019-06-10 22:45:41 -0500

    Añadida la resta

---
+++ Ejercicios/Python2.py
@@ -0,0 +1,4 @@
+#Esto no se va a leer
+print ("Loco el que me ve")
+#Esto tampoco se va a leer
+print("Esto va viento en popa")
---
+++ Ejercicios/Python3.py
@@ -0,0 +1,6 @@
+print("Empezamos con los numeros")
+num1 = 1    
+num2 = 45
+mul = num1 * num2
+print ("Multiplica parece " , mul)
+print ("Suman",(num1+num2))
