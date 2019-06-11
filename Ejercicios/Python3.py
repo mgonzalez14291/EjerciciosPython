5cbf9452f94aa60aea8a8cb4505639cb98f936da
MichaelGonzalez <mgonzalez14291@gmail.com>
2019-06-10 22:53:23 -0500

    True or false

--- Ejercicios/Python3.py
+++ Ejercicios/Python3.py
@@ -4,3 +4,6 @@ num2 = 45
 mul = num1 * num2
 print ("Multiplica parece " , mul)
 print ("Suman",(num1+num2))
+print ("Restan",(num2-num1))
+print ("5+4 > 7+8")
+print ("La anterior operacion obtiene como respuesta: ", (5+4>7+8))
