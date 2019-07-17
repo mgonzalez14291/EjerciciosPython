ab4db0a1bd7f4342f4415068659681fef2f02054
MichaelGonzalez <mgonzalez14291@gmail.com>
2019-07-17 11:12:27 -0500

    --- Auto-Git Commit ---

---
+++ Python4.py
@@ -0,0 +1,20 @@
+carros = 100
+espacio_entre_carros = 4.0
+conductores = 30
+pasajeros = 90
+carros_no_conducidos = carros - conductores
+carros_conducidos = conductores
+capacidad_loteCarros = carros_conducidos * espacio_entre_carros
+promedio_pasajeros_por_carros = pasajeros / carros_conducidos
+
+#Mensajes que serán presentados
+#(git commit -am "texto") --> para agregar ultimos cambios
+#Otro comentario
+#Comprobando git
+
+print("Existen ", carros, " carros disponibles.")
+print("Solo hay ", conductores, " conductores.")
+print("Deben haber ",carros_no_conducidos," carros vacios hoy.")
+print("Podemos transportar ",capacidad_loteCarros," personas hoy.")
+print("Tenemos ",pasajeros," pasajeros hoy.")
+print("Necesitamos hoy poner ",promedio_pasajeros_por_carros," pasajeros en cada carro.")
